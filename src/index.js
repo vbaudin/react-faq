de563288679baa866cb7d09e2532a7d1136b012f
vbaudin <vbaudin.dev@gmail.com>
2023-06-13 15:52:29 +0200

    fix: fix de certaines propriétés de style afin de mieux coller a la maquette

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./pages/App/App";
+import Header from "./components/Header/Header";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Header />
     <App />
   </React.StrictMode>
 );
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -5,7 +5,6 @@ import s from "./App.module.css";
 
 import Datas from "../../questions.json";
 import FAQCat from "../../components/FAQCat/FAQCat";
-import Header from "../../components/Header/Header";
 
 function App() {
   const [category, setCategory] = useState("<All />");
@@ -13,48 +12,49 @@ function App() {
   const categories = R.map(({ name }) => name, Datas);
 
   return (
-    <div className={s.app}>
-      <Header />
-      <h1 className={s.title}>Besoin de code clair ?</h1>
-      <h2 className={s.subtitle}>Interrogez-moi avec les bonnes balises !</h2>
-      <div className={s.container}>
-        <div className={s.aside}>
-          <span
-            className={`${s.cat_button} + ${
-              category === "<All />" && s.cat_active
-            }`}
-            onClick={() => {
-              setCategory("<All />");
-            }}
-          >
-            {"<All />"}
-          </span>
-          {R.map((cat) => {
-            return (
-              <span
-                key={cat + "btn"}
-                className={`${s.cat_button} + ${
-                  category === cat && s.cat_active
-                }`}
-                onClick={() => {
-                  setCategory(cat);
-                }}
-              >
-                {cat}
-              </span>
-            );
-          }, categories)}
-        </div>
-        <div className={s.content}>
-          {R.map((cat) => {
-            return (
-              <FAQCat
-                key={`${cat.id} ${cat.name}`}
-                cat={cat}
-                category={category}
-              />
-            );
-          }, Datas)}
+    <div className={s.page}>
+      <div className={s.app}>
+        <h1 className={s.title}>Besoin de code clair ?</h1>
+        <h2 className={s.subtitle}>Interrogez-moi avec les bonnes balises !</h2>
+        <div className={s.container}>
+          <div className={s.aside}>
+            <span
+              className={`${s.cat_button} + ${
+                category === "<All />" && s.cat_active
+              }`}
+              onClick={() => {
+                setCategory("<All />");
+              }}
+            >
+              {"<All />"}
+            </span>
+            {R.map((cat) => {
+              return (
+                <span
+                  key={cat + "btn"}
+                  className={`${s.cat_button} + ${
+                    category === cat && s.cat_active
+                  }`}
+                  onClick={() => {
+                    setCategory(cat);
+                  }}
+                >
+                  {cat}
+                </span>
+              );
+            }, categories)}
+          </div>
+          <div className={s.content}>
+            {R.map((cat) => {
+              return (
+                <FAQCat
+                  key={`${cat.id} ${cat.name}`}
+                  cat={cat}
+                  category={category}
+                />
+              );
+            }, Datas)}
+          </div>
         </div>
       </div>
     </div>
